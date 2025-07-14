4d7e7e42f62ee559f5eca8398725cbead6270b7c
Brian LaFond <52360893+iroxusux@users.noreply.github.com>
2025-07-14 14:48:49 -0400

    Add missing data argument for PyInstaller in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 source ./build.sh
 
-pyinstaller --name "Pyrox" --noconfirm --noconsole --onedir --icon="pyrox/ui/icons/_def.ico" --add-data "pyrox/ui/icons:pyrox/ui/icons" --add-data "pyrox/tasks:pyrox/tasks"  __main__.py
+pyinstaller --name "Pyrox" --noconfirm --noconsole --onedir --icon="pyrox/ui/icons/_def.ico" --add-data "pyrox/ui/icons:pyrox/ui/icons" --add-data "pyrox/tasks:pyrox/tasks" --add-data "pyrox/applications/mod:pyrox/applications/mod"  __main__.py
 
  read -p "Press Enter to continue..."
