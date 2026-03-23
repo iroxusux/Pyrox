b45f68af2282b1b9f2f29b598c9654238e06e98f
Brian LaFond <52360893+iroxusux@users.noreply.github.com>
2026-03-23 09:03:33 -0400

    feat: update version to 3.4.20, add FloorSceneObject class, and include it in the topdown assets module

--- pyrox/models/scene/assets/topdown/__init__.py
+++ pyrox/models/scene/assets/topdown/__init__.py
@@ -1,10 +1,12 @@
 from ._compkinemetic import CompositeKinematicSceneObject
+from .floor import FloorSceneObject
 from .piston import PistonSceneObject
 from .sliding_door import SlidingDoorSceneObject
 from .sensor import SensorSceneObject
 
 __all__ = [
     "CompositeKinematicSceneObject",
+    "FloorSceneObject",
     "PistonSceneObject",
     "SlidingDoorSceneObject",
     "SensorSceneObject",
